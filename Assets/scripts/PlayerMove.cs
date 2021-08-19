40e07ebe0379201d99c21ee58683030b15cbab1f
Keith Jerosky <kjerosky@gmail.com>
2021-08-18 23:56:46 -0700

    Implement running for player

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-    public float walkSpeed = 3;
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
 
     private const float GRAVITY = -9.8f;
 
@@ -16,11 +17,16 @@ public class PlayerMove : MonoBehaviour {
     }
 
     private void handleMovement() {
+        float movementSpeed = walkSpeed;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            movementSpeed = runSpeed;
+        }
+
         float strafeMove = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
         float forwardMove = (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
 
-        strafeMove *= walkSpeed * Time.deltaTime;
-        forwardMove *= walkSpeed * Time.deltaTime;
+        strafeMove *= movementSpeed * Time.deltaTime;
+        forwardMove *= movementSpeed * Time.deltaTime;
 
         Vector3 movement = transform.right * strafeMove + transform.forward * forwardMove;
         characterController.Move(movement);
