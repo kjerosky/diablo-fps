45cc46f5d50be4d1454492138d586a33776b4d14
Keith Jerosky <kjerosky@gmail.com>
2021-08-18 21:52:34 -0700

    Implement gravity force on player

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -6,13 +6,16 @@ public class PlayerMove : MonoBehaviour {
 
     public float walkSpeed = 3;
 
+    private const float GRAVITY = -9.8f;
+
     private CharacterController characterController;
+    private Vector3 verticalVelocity;
 
     void Start() {
         characterController = GetComponent<CharacterController>();
     }
 
-    void Update() {
+    private void handleMovement() {
         float strafeMove = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
         float forwardMove = (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
 
@@ -21,5 +24,19 @@ public class PlayerMove : MonoBehaviour {
 
         Vector3 movement = transform.right * strafeMove + transform.forward * forwardMove;
         characterController.Move(movement);
+    }
+
+    private void handleGravity() {
+        verticalVelocity.y += GRAVITY * Time.deltaTime;
+        characterController.Move(verticalVelocity * Time.deltaTime);
+
+        if (characterController.isGrounded) {
+            verticalVelocity.y = -0.1f;
+        }
+    }
+
+    void Update() {
+        handleMovement();
+        handleGravity();
     }
 }
