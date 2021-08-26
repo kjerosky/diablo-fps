e7201a2c8024d72c26113231143667d26fa96cab
Keith Jerosky <kjerosky@gmail.com>
2021-08-25 20:53:23 -0700

    Add arrow firing functionality to bow with animations

--- Assets/scripts/BowEventHandler.cs
+++ Assets/scripts/BowEventHandler.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class BowEventHandler : MonoBehaviour {
 
     public WeaponManager weaponManager;
+    public GameObject arrowPrefab;
+    public Transform arrowLaunchPoint;
+    public float arrowInitialVelocity = 40;
 
     public void SignalBowWasPutAway() {
         weaponManager.switchToNextSelectedWeapon();
+    }
+
+    public void FireArrow() {
+        GameObject newArrow = Instantiate(arrowPrefab, arrowLaunchPoint.position, arrowLaunchPoint.rotation);
+
+        Vector3 initialVelocity = newArrow.transform.forward * arrowInitialVelocity;
+
+        Rigidbody rb = newArrow.GetComponent<Rigidbody>();
+        rb.AddForce(initialVelocity, ForceMode.VelocityChange);
+
+        Quaternion rotationTowardsVelocity = Quaternion.LookRotation(initialVelocity.normalized);
+        newArrow.transform.rotation = rotationTowardsVelocity;
     }
 }
--- Assets/scripts/SimpleBow.cs
+++ Assets/scripts/SimpleBow.cs
@@ -5,25 +5,10 @@ using UnityEngine;
 public class SimpleBow : Weapon {
 
     public Animator animator;
-    public GameObject arrowPrefab;
-    public Transform arrowLaunchPoint;
-    public float arrowInitialVelocity = 1;
 
     void Update() {
         bool firePressed = Input.GetMouseButton(0) || Input.GetKey(KeyCode.J);
         animator.SetBool("FirePressed", firePressed);
-
-        // if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.J)) {
-        //     GameObject newArrow = Instantiate(arrowPrefab, arrowLaunchPoint.position, arrowLaunchPoint.rotation);
-
-        //     Vector3 initialVelocity = newArrow.transform.forward * arrowInitialVelocity;
-
-        //     Rigidbody rb = newArrow.GetComponent<Rigidbody>();
-        //     rb.AddForce(initialVelocity, ForceMode.VelocityChange);
-
-        //     Quaternion rotationTowardsVelocity = Quaternion.LookRotation(initialVelocity.normalized);
-        //     newArrow.transform.rotation = rotationTowardsVelocity;
-        // }
     }
 
     public override void putAway() {
