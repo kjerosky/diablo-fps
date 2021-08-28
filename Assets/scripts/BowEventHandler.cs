cf32a8853f86840be5c511013c605714fd1329ae
Keith Jerosky <kjerosky@gmail.com>
2021-08-28 13:52:40 -0700

    Add bow sounds

--- Assets/scripts/BowEventHandler.cs
+++ Assets/scripts/BowEventHandler.cs
@@ -8,6 +8,30 @@ public class BowEventHandler : MonoBehaviour {
     public GameObject arrowPrefab;
     public Transform arrowLaunchPoint;
     public float arrowInitialVelocity = 40;
+    public AudioClip bowReadySound;
+    public AudioClip bowNockArrowSound;
+    public AudioClip bowPullSound;
+    public AudioClip bowReleaseSound;
+
+    private AudioSource bowReadyAudioSource;
+    private AudioSource bowNockArrowAudioSource;
+    private AudioSource bowPullAudioSource;
+    private AudioSource bowReleaseAudioSource;
+
+    void Awake() {
+        bowReadyAudioSource = gameObject.AddComponent<AudioSource>();
+        bowReadyAudioSource.clip = bowReadySound;
+        bowReadyAudioSource.playOnAwake = false;
+        bowNockArrowAudioSource = gameObject.AddComponent<AudioSource>();
+        bowNockArrowAudioSource.clip = bowNockArrowSound;
+        bowNockArrowAudioSource.playOnAwake = false;
+        bowPullAudioSource = gameObject.AddComponent<AudioSource>();
+        bowPullAudioSource.clip = bowPullSound;
+        bowPullAudioSource.playOnAwake = false;
+        bowReleaseAudioSource = gameObject.AddComponent<AudioSource>();
+        bowReleaseAudioSource.clip = bowReleaseSound;
+        bowReleaseAudioSource.playOnAwake = false;
+    }
 
     public void SignalBowWasPutAway() {
         weaponManager.switchToNextSelectedWeapon();
@@ -23,5 +47,21 @@ public class BowEventHandler : MonoBehaviour {
 
         Quaternion rotationTowardsVelocity = Quaternion.LookRotation(initialVelocity.normalized);
         newArrow.transform.rotation = rotationTowardsVelocity;
+    }
+
+    public void playReadySound() {
+        bowReadyAudioSource.Play();
+    }
+
+    public void playNockArrowSound() {
+        bowNockArrowAudioSource.Play();
+    }
+
+    public void playPullSound() {
+        bowPullAudioSource.Play();
+    }
+
+    public void playReleaseSound() {
+        bowReleaseAudioSource.Play();
     }
 }
